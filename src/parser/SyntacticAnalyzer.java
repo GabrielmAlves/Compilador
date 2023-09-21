924488c79df7141f1a4fd8ccdc0c81de792884d5
GabrielmAlves <gabrielmalves10@gmail.com>
2023-09-21 10:59:20 -0300

    criando branch do sintatico

---
+++ src/parser/SyntacticAnalyzer.java
@@ -0,0 +1,18 @@
+package parser;
+
+import lexical.Token;
+import lexical.LexicalAnalyzer;
+
+public class SyntacticAnalyzer {
+	
+	private Token token;
+	private LexicalAnalyzer claudio = new LexicalAnalyzer();
+	
+	public SyntacticAnalyzer(Token tokinho){
+		this.token = tokinho;
+	}
+	
+	public void Analyzer(){
+		
+	}
+}
