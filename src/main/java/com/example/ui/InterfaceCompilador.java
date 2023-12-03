66805e7056b66278ba84386446b22ae6049b5150
Rafa_Dorta <rafael.alves.dorta@gmail.com>
2023-12-02 23:28:47 -0300

    CSS Interface Grafica

--- src/main/java/com/example/ui/InterfaceCompilador.java
+++ src/main/java/com/example/ui/InterfaceCompilador.java
@@ -6,15 +6,18 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class InterfaceCompilador extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(InterfaceCompilador.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 500);
+
         stage.setTitle("Compilador");
         stage.setScene(scene);
         stage.show();
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("Style.css")).toExternalForm());
     }
 
     public static void main(String[] args) {
